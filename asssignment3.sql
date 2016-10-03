5217d489c43a43ff04eecc8c7907b5c27193e383
Sheela Aryal <Sheela@MacBook-Pro.home>
2016-10-03 11:01:08 -0400

    sql queries updated
    updated assignment 3 queries

--- asssignment3.sql
+++ asssignment3.sql
@@ -1,3 +1,4 @@
+/* Question 1 */
 SELECT x.userId, fname, lname FROM
 user AS x 
 JOIN
@@ -8,40 +9,46 @@ INNER JOIN
 ON a.skillId = b.skillId) AS y
 ON x.userId = y.userId;
 
-
+/* Question 2 */
 SELECT z.fname, z.lname, skills.name, z.c FROM
 (SELECT x.fname, x.lname, y.skillId, y.c FROM
 user AS x
 JOIN
-(select toUserid, skillId, Count(*) AS c
-from endorsement
-group by toUserid, skillId
-having c >= 10) AS y
+(SELECT toUserid, skillId, Count(*) AS c
+FROM endorsement
+GROUP BY toUserid, skillId
+HAVING c > 10) AS y
 ON x.userId = y.toUserid) AS z
 JOIN skills
 ON z.skillId=skills.skillId;
 
 
-
+/* Question 3 */
 SELECT user.fname, user.lname, a.duration AS NoOfYears FROM 
 (SELECT userId, SUM(DATEDIFF(toDate, fromDate)/365) AS duration FROM Experience GROUP BY userId HAVING duration >= 10) AS a
 JOIN user
 ON a.userId = user.userId;
 
 
-
+/*Question 4 */
 SELECT user.fname, user.lname, a.c AS Connections FROM 
 (SELECT fromUserid, COUNT(toUserid) AS c FROM connections GROUP BY fromUserid HAVING c > 15) AS a JOIN
 user ON a.fromUserid = user.userId; 
 
+/*Question 5 */
+SELECT *FROM user WHERE year(dob)> 1956 AND year(dob)<2016;
 
+/*Question 6 */
+DELETE FROM user WHERE userId NOT IN 
+(SELECT DISTINCT userId FROM Experience);
 
+/*Question 7 */
+SELECT userId,fname, lname, dateReg FROM user WHERE dateReg <= DATE_SUB(NOW(),INTERVAL 2 YEAR);
+
+/* Question 8 */
 SELECT user.fname, user.lname FROM
 user JOIN
 (SELECT DISTINCT userId FROM userAwards) AS a 
 ON user.userId = a.userId;
 
 
-
-DELETE FROM user WHERE userId NOT IN 
-(SELECT DISTINCT userId FROM Experience);
